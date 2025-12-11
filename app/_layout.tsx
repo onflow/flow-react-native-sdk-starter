49699080ed0c04cb85dc1c015ee921d11d4483e7
mfbz <mfbz@users.noreply.github.com>
2025-12-11 18:26:54 +0100

    Fixed edges

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { FlowProviderWrapper } from "@/components/flow-provider-wrapper";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
 export default function RootLayout() {
   return (
     <FlowProviderWrapper>
-      <Stack screenOptions={{ headerShown: false }} />
-      <StatusBar style="auto" />
+      <View style={{ flex: 1, backgroundColor: "#fff" }}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </View>
+      <StatusBar style="dark" />
     </FlowProviderWrapper>
   );
 }
